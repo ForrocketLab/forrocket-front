7f77c044852929cf25c1880225b9a4c7f039c8c4
gabsvelozo <gabsmastellari@gmail.com>
2025-06-18 16:06:03 -0300

    fix: correct import path for LoginPage and add EvaluationCicle route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/login/Login';
+import LoginPage from './pages/login/login';
 import { AuthProvider } from './contexts/AuthProvider';
 import HomePage from './pages/home/Home';
+import EvaluationCicle from './pages/evaluation/EvaluationCicle';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path='/evaluation' element={<EvaluationCicle />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
