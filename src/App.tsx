981488bb90544016b05b018e2e89f2c31cc4695f
thaisnevest <thaisneves@citi.org.br>
2025-06-17 21:21:21 -0300

    feat(feat/360-mentor-evaluation): added sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/login/Login';
+import LoginPage from './pages/login/login';
 import { AuthProvider } from './contexts/AuthProvider';
 import HomePage from './pages/home/Home';
+import Sidebar from './components/Sidebar';
+import EvaluationPage from './pages/evaluation/evaluation';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <Sidebar />
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/avaliacao' element={<EvaluationPage />} />
           <Route path='/login' element={<LoginPage />} />
         </Routes>
       </AuthProvider>
