ab0b6e87c0d88715bb75d6fadc6930a5d6931a6e
Guilherme Trajano <145384640+guigotrajano@users.noreply.github.com>
2025-06-19 19:56:30 -0300

    feat: Integração - referências de colaborador

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/login/login';
+import LoginPage from './pages/login/Login';
 import { AuthProvider } from './contexts/AuthProvider';
 import HomePage from './pages/home/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 import { ROLES } from './types/roles';
 import MainLayout from './components/MainLayout';
 import NotFoundPage from './pages/not-found/NotFound';
+import RefPage from './pages/referencias/RefCollaborator';
+
 
 function App() {
   return (
@@ -15,35 +17,32 @@ function App() {
           {/* Rotas Públicas */}
           <Route path='/login' element={<LoginPage />} />
           <Route path='/unauthorized' element={<h1>Não autorizado</h1>} />
+          
 
-          {/* Rotas protegidas com layout padrão (SideMenu incluso) */}
-          <Route element={<MainLayout />}>
-            <Route element={<ProtectedRoute allowedRoles={[ROLES.ADMIN]} />}>
-              <Route path='/admin' element={<HomePage />} />
-            </Route>
+          {/* Fluxo do Admin */}
+          <Route element={<ProtectedRoute allowedRoles={[ROLES.ADMIN]} />}>
+            <Route path='/admin' element={<HomePage />} />
+          </Route>
 
             <Route element={<ProtectedRoute allowedRoles={[ROLES.COLLABORATOR]} />}>
               <Route path='/' element={<HomePage />} />
+              <Route path='/avaliacao' element={<RefPage />} />
+
             </Route>
 
-            <Route element={<ProtectedRoute allowedRoles={[ROLES.RH]} />}>
-              <Route path='/rh' element={<h1>RH</h1>} />
-            </Route>
+          {/* Fluxo do RH */}
+          <Route element={<ProtectedRoute allowedRoles={[ROLES.RH, ROLES.ADMIN]} />}>
+            <Route path='/rh' element={<h1>RH</h1>} />
+          </Route>
 
-            <Route element={<ProtectedRoute allowedRoles={[ROLES.COMMITTEE]} />}>
-              <Route path='/commitee' element={<h1>Comitê</h1>} />
-            </Route>
+          {/* Fluxo do Colaborador */}
+          <Route element={<ProtectedRoute allowedRoles={[ROLES.COMMITTEE, ROLES.ADMIN]} />}>
+            <Route path='/commitee' element={<h1>RH</h1>} />
+          </Route>
 
-            <Route element={<ProtectedRoute allowedRoles={[ROLES.MANAGER]} />}>
-              <Route path='/manager' element={<h1>Gestor</h1>} />
-            </Route>
-
-            <Route element={<ProtectedRoute allowedRoles={[ROLES.COLLABORATOR]} />}>
-              <Route path='/dashboard' element={<h1>Gestor</h1>} />
-            </Route>
-
-            {/* ROTA DE FALLBACK (404) */}
-            <Route path='*' element={<NotFoundPage />} />
+          {/* Fluxo do Gestor */}
+          <Route element={<ProtectedRoute allowedRoles={[ROLES.MANAGER, ROLES.ADMIN]} />}>
+            <Route path='/manager' element={<h1>RH</h1>} />
           </Route>
         </Routes>
       </AuthProvider>
